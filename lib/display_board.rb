9142d7dc28ce0d705bccdbad2723223d733413b8
Harry Bloch <harryfbloch@gmail.com>
2018-09-01 19:54:40 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,5 +9,5 @@ def line
 end
 
 def display_board(gameArray)
- puts row(gameArray[0],gameArray[1],gameArray[2]) + line + row + line + row
+ puts row(gameArray[0],gameArray[1],gameArray[2]) + line + row(gameArray[3],gameArray[4],gameArray[5]) + line + row(gameArray[6],gameArray[7],gameArray[8])
 end
