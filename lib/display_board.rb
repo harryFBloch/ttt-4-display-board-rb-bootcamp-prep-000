234f6720e775c33dc1f9689ea8e2cf14523114e4
Harry Bloch <harryfbloch@gmail.com>
2018-09-01 19:53:54 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def row(a = " ", b = " ", c = " ") 
+  " #{a} " + "|" +" #{b} " +  "|" + " #{c} \n"
+end
+
+def line 
+  "-----------\n"
+end
+
+def display_board(gameArray)
+ puts row(gameArray[0],gameArray[1],gameArray[2]) + line + row + line + row
+end
